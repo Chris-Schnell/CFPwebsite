b2320a0ba0c1578ebbeed403c54d83929b45a4bf
Tyler Schnell <tylerschnell123@gmail.com>
2023-10-15 09:53:27 -0700

    linksUpdate

--- src/Artists.js
+++ src/Artists.js
@@ -14,7 +14,7 @@ function Artists() {
             <div className="indArtistContainer">
                 <div className='artistTitleBar'>Night City</div>
                 <div className='artistTitle'>Night City</div>
-                <a className='artistButtons' href="https://soundcloud.com/nightcity0x"><span style={{"textDecoration": "underline"}}>L</span>isten on Soundcloud</a>
+                <a className='artistButtons' href="https://soundcloud.com/nightcity0x/perception?si=3da14ef636e54ddfba51d9e80865e272&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing#t=0%3A08"><span style={{"textDecoration": "underline"}}>L</span>isten on Soundcloud</a>
             </div>
             
             <br></br>
@@ -22,7 +22,7 @@ function Artists() {
             <div className="indArtistContainer">
             <div className='artistTitleBar'>Boat Gang Ent</div>
             <div className='artistTitle'>Boat Gang Ent</div>
-            <a className='artistButtons' href="https://www.youtube.com/@BoatGangEnt/videos">Watch on <span style={{"textDecoration": "underline"}} >Y</span>outube</a>
+            <a className='artistButtons' href="https://youtu.be/6nD1KWs_SIo?si=jwdO_ruG8YRZpPD_">Watch on <span style={{"textDecoration": "underline"}} >Y</span>outube</a>
             </div>
             
             </div>
